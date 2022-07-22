0c0c923a024b857f60405aa76b81a515e668c05a
SangHuPark <tkdgn5408407@gmail.com>
2022-07-22 17:56:32 +0900

    API Release

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ sequelize.sync({ force: false })
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
 app.use('/login', loginRouter);
 app.use('/enroll', enrollRouter);
 app.use('/todo', todoRouter);
